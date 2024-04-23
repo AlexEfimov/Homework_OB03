79c4b57886073165aeb93bbc7e6f7cd9e5934a3e
AlexEfimov <alex@efimov.mobi>
2024-04-24 00:04:35 +0300

    zoo created

--- main.py
+++ main.py
@@ -11,40 +11,103 @@ class Animal():
 
 class Bird(Animal):
     def make_sound(self):
-        print(f"{self.name} is singing")
+        print(f"{self.name} tweets")
 
 class Mammal(Animal):
     def make_sound(self):
-        print(f"{self.name} is roaring")
+        print(f"{self.name} roars")
 
 class Reptile(Animal):
     def make_sound(self):
-        print(f"{self.name} is hiss-hiss")
+        print(f"{self.name} hiss-hiss")
 
-animals = [Bird("Nightingale",10), Mammal("Lion",5), Reptile("Snake",4)]
-for animal in animals:
-    animal.make_sound()
 
-class Humans():
-    def __init__(self, name, gender, age):
+class Human():
+    def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.gender = gender
 
-    def job(self):
-        pass
 
-class ZooKeeper(Humans):
+class ZooKeeper(Human):
     def feed_animal(self, animal):
         print(f"{self.name} is feeding {animal.name}")
 
-class Veterinarian(Humans):
+class Veterinarian(Human):
     def heal_animal(self, animal):
         print(f"{self.name} is healing {animal.name}")
 
-Person1=Veterinarian("John","male",30)
-Person2=ZooKeeper("Anna","female",25)
 
-for animal in animals:
-    Person1.heal_animal(animal)
-    Person2.feed_animal(animal)
+class Zoo():
+    def __init__(self):
+        self.animals = []
+        self.staff = []
+
+
+    def add_animal(self, animal):
+        self.animals.append(animal)
+
+
+    def remove_animal(self, animal):
+        if animal in self.animals:
+            self.animals.remove(animal)
+        else:
+            print("No such animal")
+
+
+
+    def add_staff(self, staff):
+        self.staff.append(staff)
+
+    def remove_staff(self, staff):
+        if staff in self.staff:
+            self.staff.remove(staff)
+        else:
+            print("No such staff member")
+
+
+    def animals_info(self):
+        for animal in self.animals:
+            print(f"Animal {animal.name} of age {animal.age} is {animal.__class__.__name__}")
+
+    def staff_info(self):
+        for staff in self.staff:
+            print(f" Staff member {staff.name} of age {staff.age} is {staff.__class__.__name__} ")
+
+zoo = Zoo()
+
+animal1 = Bird("Kesha", 8)
+animal2 = Mammal("Barsik", 5)
+animal3 = Reptile("Nag", 3)
+animal4 = Mammal("Tuzik", 6)
+animal5 = Mammal("Ashley",3)
+
+staff1 = Veterinarian("Petruccio",30)
+staff2 = ZooKeeper("Malvina", 19)
+staff3 = ZooKeeper("Eva", 28)
+staff4 = ZooKeeper("Bob", 24)
+
+zoo.add_animal(animal1)
+zoo.add_animal(animal2)
+zoo.add_animal(animal3)
+zoo.add_animal(animal4)
+
+zoo.add_staff(staff1)
+zoo.add_staff(staff2)
+zoo.add_staff(staff3)
+
+zoo.animals_info()
+zoo.staff_info()
+
+zoo.remove_animal(animal5)
+zoo.remove_animal(animal2)
+zoo.animals_info()
+
+zoo.remove_staff(staff4)
+zoo.remove_staff(staff2)
+zoo.staff_info()
+
+
+
+
+
+
