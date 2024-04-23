f1527445a7785e2d41c46ad44447543ae336609f
AlexEfimov <alex@efimov.mobi>
2024-04-23 21:36:09 +0300

    Class Humans and parent classes added

--- main.py
+++ main.py
@@ -24,3 +24,27 @@ class Reptile(Animal):
 animals = [Bird("Nightingale",10), Mammal("Lion",5), Reptile("Snake",4)]
 for animal in animals:
     animal.make_sound()
+
+class Humans():
+    def __init__(self, name, gender, age):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def job(self):
+        pass
+
+class ZooKeeper(Humans):
+    def feed_animal(self, animal):
+        print(f"{self.name} is feeding {animal.name}")
+
+class Veterinarian(Humans):
+    def heal_animal(self, animal):
+        print(f"{self.name} is healing {animal.name}")
+
+Person1=Veterinarian("John","male",30)
+Person2=ZooKeeper("Anna","female",25)
+
+for animal in animals:
+    Person1.heal_animal(animal)
+    Person2.feed_animal(animal)
